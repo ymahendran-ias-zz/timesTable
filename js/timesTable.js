8f84c2f477252122f2863ebd1cb08732bf03113b
Yuva <ymahendran@integralads.com>
2021-02-07 12:49:23 -0500

    enhanced the library function

--- js/timesTable.js
+++ js/timesTable.js
@@ -1,20 +1,25 @@
-//initialize variables
-var timesTableHeader = "";
-var timesTable = "";
 
-for(var a = 2; a <= 7; a++ ) {
-    //form the header here: 
-    timesTableHeader += "<th>" + a + " Times Table </th>";
+function drawTimesTable(minTimes, maxTimes, iterCount) {
 
-    //form the contents here:
-    timesTable += "<td>";
-    //in this loop add content to the initialized variables
-    for(var i = 1; i <= 1000; i++) {
-        timesTable += a + " X " + i + " = " + a * i + "<br/>";
+    //initialize variables
+    var timesTableHeader = "";
+    var timesTable = "";
+
+    for(var a = minTimes; a <= maxTimes; a++ ) {
+        //form the header here: 
+        timesTableHeader += "<th>" + a + " Times Table </th>";
+
+        //form the contents here:
+        timesTable += "<td>";
+        //in this loop add content to the initialized variables
+        for(var i = 1; i <= iterCount; i++) {
+            timesTable += a + " X " + i + " = " + a * i + "<br/>";
+        }
+        timesTable += "</td>";
     }
-    timesTable += "</td>";
-}
 
-//render the contents to the page
-document.getElementById('timesTableHeader').innerHTML = timesTableHeader;
-document.getElementById('timesTableContent').innerHTML = timesTable;
+    //render the contents to the page
+    document.getElementById('timesTableHeader').innerHTML = timesTableHeader;
+    document.getElementById('timesTableContent').innerHTML = timesTable;
+
+}
